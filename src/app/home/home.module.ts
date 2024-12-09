2e19e19d097eeb53b054c50c5020f84dd0722ad7
Alae Belaich <belaich.alae@gmail.com>
2024-12-09 10:22:35 +0100

    Injecion in home.module.ts

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { DrinkRepository } from './repository/drink-repository';
+import { DrinkRepositoryDummyImplService } from './repository/drink-repository-dummy-impl.service';
 
 
 @NgModule({
@@ -14,6 +16,11 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+
+  providers: [{
+    provide : DrinkRepository, 
+    useClass : DrinkRepositoryDummyImplService,
+  }],
 })
 export class HomePageModule {}
